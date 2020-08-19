aa504dfc591be9b75fea879204ae33b2886e14aa
ShikharJ <jaiswalshikhar87@gmail.com>
2020-08-19 11:53:55 +0530

    Replace Scale Macro With A Single Typedef

--- c_reference/include/quantized_datatypes.h
+++ c_reference/include/quantized_datatypes.h
@@ -5,7 +5,7 @@
 #define __QUANTIZED_DATATYPES_H__
 
 #include <stdint.h>
-
+#define SHIFT
 // Macros for input type.
 typedef int8_t Q7_T;
 typedef int16_t Q15_T;
@@ -14,9 +14,8 @@ typedef int32_t Q31_T;
 typedef uint32_t ITER_T;
 // Macro for signed iterator type.
 typedef int32_t S_ITER_T;
-// Macros for scale variable type.
-typedef int16_t SCALE_T;
-typedef int32_t L_SCALE_T;
+// Macro for scale variable type.
+typedef int32_t SCALE_T;
 // Macro for max value of input type.
 #define Q15_TMAX 32767
 // Macro for min value of input type.
